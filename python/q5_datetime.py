5732edf0f2a6d10d251a2181b7439047b4750c88
stewartstout <stewart.stout@gmail.com>
2016-03-30 12:27:15 -0400

    Update q5_datetime.py

--- python/q5_datetime.py
+++ python/q5_datetime.py
@@ -1,13 +1,36 @@
-# Hint:  use Google to find python function
+from datetime import datetime
 
 ####a) 
-date_start = '01-02-2013'  
-date_stop = '07-28-2015'   
+
+date_start = '01-02-2013'    
+date_stop = '07-28-2015'
+
+def days_between(d1, d2):
+    d1 = datetime.strptime(d1, "%m-%d-%Y")
+    d2 = datetime.strptime(d2, "%m-%d-%Y")
+    return abs((d2 - d1).days)
+    
+print days_between(date_start,date_stop)
 
 ####b)  
 date_start = '12312013'  
-date_stop = '05282015'  
+date_stop = '05282015'
+
+def convert_to_date(d1,d2):
+	d1 = d1[0:2] + '-' + d1[2:4] + '-' + d1[4:9]
+	d2 = d2[0:2] + '-' + d2[2:4] + '-' + d2[4:9]
+	d1 = datetime.strptime(d1, "%m-%d-%Y")
+	d2 = datetime.strptime(d2, "%m-%d-%Y")
+	print abs((d2 - d1).days)    
+
+convert_to_date(date_start,date_stop)
 
 ####c)  
-date_start = '15-Jan-1994'  
-date_stop = '14-Jul-2015'  
+
+from datetime import datetime
+
+d1 = '15-Jan-1994'      
+d2 = '14-Jul-2015'
+d1 = datetime.strptime(d1, "%d-%b-%Y")
+d2 = datetime.strptime(d2, "%d-%b-%Y")
+print abs((d2 - d1).days)
